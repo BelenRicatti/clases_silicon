d41df9cd0ecfc23320203b72d31b82e83bf9d1de
lucaspodkowa <lucaspodkowa@gmail.com>
2023-05-17 20:00:11 -0300

    agregue un punto y coma

--- ejercicio19.js
+++ ejercicio19.js
@@ -2,4 +2,4 @@ let variable = "ta ta ta ta";
 let variable_2 = 5;
 
 var otra_variable = "otro contenido agregado por lucas"
-let variable_de_mañana = "cosas que hice otro dia"
+let variable_de_mañana = "cosas que hice otro dia";
